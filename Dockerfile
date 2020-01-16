986b6c1fc972c1c40e79be6e89db80d30cca8241
Michal Vala <mvala@redhat.com>
2020-01-16 11:10:13 +0100

    format and cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM golang:1.13 AS builder
 WORKDIR /go/src
-RUN pwd
 COPY dummylogger.go .
-RUN ls -l
 RUN go build -o /go/bin/dummylogger dummylogger.go
-RUN ls -l
 
 FROM alpine:3
 COPY --from=builder /go/bin/dummylogger .
--- dummylogger.go
+++ dummylogger.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "time"
+	"flag"
+	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
-    conf := parseArgs()
+	conf := parseArgs()
 
-    if conf.endAfterSeconds >= 0 {
-        time.AfterFunc(time.Duration(conf.endAfterSeconds)*time.Millisecond, func() {
-            os.Exit(0)
-        })
-    }
+	if conf.endAfterSeconds >= 0 {
+		time.AfterFunc(time.Duration(conf.endAfterSeconds)*time.Millisecond, func() {
+			os.Exit(0)
+		})
+	}
 
-    if conf.failAfterSeconds >= 0 {
-        time.AfterFunc(time.Duration(conf.failAfterSeconds)*time.Millisecond, func() {
-            os.Exit(1)
-        })
-    }
+	if conf.failAfterSeconds >= 0 {
+		time.AfterFunc(time.Duration(conf.failAfterSeconds)*time.Millisecond, func() {
+			os.Exit(1)
+		})
+	}
 
-    var logFile *os.File
-    if len(conf.logToFilePath) > 0 {
-        var err error
-        if logFile, err = os.Create(conf.logToFilePath); err != nil {
-            panic(err)
-        }
-        defer logFile.Close()
-    }
+	var logFile *os.File
+	if len(conf.logToFilePath) > 0 {
+		var err error
+		if logFile, err = os.Create(conf.logToFilePath); err != nil {
+			panic(err)
+		}
+		defer logFile.Close()
+	}
 
-    ticker := time.NewTicker(time.Duration(conf.logInterval) * time.Millisecond)
-    for range ticker.C {
-        now := time.Now().UnixNano()
-        _, _ = fmt.Fprintf(os.Stdout, "[%d] stdout\n", now)
-        _, _ = fmt.Fprintf(os.Stderr, "[%d] stderr\n", now)
-        _, _ = fmt.Fprintf(logFile, "[%d] file\n", now)
-    }
+	ticker := time.NewTicker(time.Duration(conf.logInterval) * time.Millisecond)
+	for range ticker.C {
+		now := time.Now().UnixNano()
+		_, _ = fmt.Fprintf(os.Stdout, "[%d] stdout\n", now)
+		_, _ = fmt.Fprintf(os.Stderr, "[%d] stderr\n", now)
+		_, _ = fmt.Fprintf(logFile, "[%d] file\n", now)
+	}
 }
 
 func parseArgs() *conf {
-    var conf = &conf{}
+	var conf = &conf{}
 
-    flag.IntVar(&conf.failAfterSeconds, "fail", -1, "number of seconds after program should fail with !=0")
-    flag.IntVar(&conf.endAfterSeconds, "end", -1, "number of seconds after program should end with 0")
-    flag.StringVar(&conf.logToFilePath, "file", "", "path of the file to log into")
-    flag.IntVar(&conf.logInterval, "loginterval", 100, "log each <loginterval> ms")
+	flag.IntVar(&conf.failAfterSeconds, "fail", -1, "number of seconds after program should fail with !=0")
+	flag.IntVar(&conf.endAfterSeconds, "end", -1, "number of seconds after program should end with 0")
+	flag.StringVar(&conf.logToFilePath, "file", "", "path of the file to log into")
+	flag.IntVar(&conf.logInterval, "loginterval", 100, "log each <loginterval> ms")
 
-    flag.Parse()
+	flag.Parse()
 
-    fmt.Printf("%+v\n", *conf)
+	fmt.Printf("%+v\n", *conf)
 
-    return conf
+	return conf
 }
 
 type conf struct {
-    failAfterSeconds int
-    endAfterSeconds  int
-    logInterval      int
-    logToFilePath    string
+	failAfterSeconds int
+	endAfterSeconds  int
+	logInterval      int
+	logToFilePath    string
 }
